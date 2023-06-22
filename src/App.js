f3ff95e51117b2ddcf95812a62b775520e6aa16a
baevolution <baevolutiond@gmail.com>
2023-06-23 00:02:10 +0900

    코딩애플 리액트 강의 복습 세팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [title, setTitle] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [like, setLike] = useState(0);
+  let [modal, setModal] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h4 >블로그 복습</h4>
       </header>
-    </div>
-  );
+
+      <button onClick={() => {
+        let copyTitle = [...title];
+        copyTitle[0] = '여자 코트 추천';
+        setTitle(copyTitle);
+      }}>
+        수정 버튼
+      </button>
+      <button onClick={() => {
+        let copyTitle = [...title];
+        copyTitle.sort();
+        setTitle(copyTitle);
+      }}>
+        가나다 순 정렬
+      </button>
+
+      <div className="list">
+        <h4 onClick={() => {
+          setModal(!modal)
+        }}>
+          {title[0]}
+          <span onClick={() => {
+            setLike(like + 1);
+          }}> 👍🏻</span> {like}
+        </h4>
+        {
+          modal === true ? <Modal /> : null
+        }
+        <p>발행 날짜</p>
+      </div>
+
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>발행 날짜</p>
+      </div>
+
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>발행 날짜</p>
+      </div>
+
+
+
+    </div >
+  )
+}
+
+function Modal() {
+  return (
+    <>
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+      <div></div>
+    </> // fragment 문법
+  )
 }
 
 export default App;
